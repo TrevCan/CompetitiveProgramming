b6e672620777675b20974e116b11f057abf996e3
TrevCan <heror0484@gmail.com>
2020-12-27 20:44:03 -0600

    finished up to Permutations

--- cses.fi/004permutations.cpp
+++ cses.fi/004permutations.cpp
@@ -1,27 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isBefore(int a, int b){
+bool compatible(int a, int b){
 	//return a > b;
-	if(abs(b-a) != 1){
-		cout << "true " << a << "  " << b << "\n";
-	}
 	return abs(b-a) != 1;
 }
 
 int main() {
 	int n;
 	cin >> n;
-	signed int numbers[n];
+	int numbers[n];
+//	cout << sizeof(numbers)/sizeof(numbers[0])<<"\n";
 
-	for(int i = 0; i < n; i++){
-		numbers[i] = i+1;
+	if(n == 1){ cout << 1; return 0;}
+	if(n < 4){
+		cout << "NO SOLUTION";
+		return 0;
 	}
+	//split into halves
+	int lFirst = ceil((double)n / 2), lSecond = n / 2;
+//	cout << " f " << lFirst << "\n";
+//	cout << " s " << lSecond << "\n";
+	int number;
+	if(n > 4)
+		number = 1;
+	else
+		number = 2;
+	for(int i = 0; i < lFirst; i++){
+		numbers[i] = number;
+		number += 2;
+	}
+//	cout << lFirst << "\n";
 
-	sort(numbers, numbers+n, isBefore);
-
-	for(int i = 0; i < n; i++){
+	if(n>4)
+		number = 2;
+	else
+		number = 1;
+	for(int i = 0; i < lSecond; i++){
+//			cout << "hi\n";
+		numbers[i+lFirst] = number;
+		number += 2;
+	}
+	
+	for(int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++){
 		cout << numbers[i] << " ";
 	}
+
 	return 0;
+
 }
