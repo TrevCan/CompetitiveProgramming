3119461df57aad9fca924f5cf908b270ddd53df7
TrevCan <heror0484@gmail.com>
2021-01-10 00:03:39 -0600

    added problem H

--- contests/2020_las_12_uvas/h.caballoVignedo.cpp
+++ contests/2020_las_12_uvas/h.caballoVignedo.cpp
@@ -19,59 +19,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int mins, goal; int c; int sum;
+long long int mins, goal, c, sum;
 
 int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
 	cin >> c >> goal;
 	while(c != 0){
-		int rac[c];
-		for(int i=0; i < c; i++){
+		long long int rac[c];
+		for(long long int i=0; i < c; i++){
 //			cout << "WAITING for in...\n";
-			int input;
+			long long int input;
 			cin >> input;
 			rac[i] = input;
 		}
-		int p1=0, p2=1;
-		sum = rac[0];
+		long long int p2=0;
+
 		if(rac[0] == goal){
 			c = 0;
 			mins = goal;
 		}
-		for(int i = 0; i < c; i++){
-			if(i == c-1){
+		for(long long int i = 0; i < c; i++){
+			if(i == c-1 && rac[i] >= goal){
 				mins = min(mins,rac[c-1]);
 				break;
 			}
-			while(sum < goal){
-				cout << "YOLO\n";
-				if(p2>c-1){
-					break;
-				}
+			while(sum < goal && p2 < c){	
 				sum += rac[p2];
+//				cout << "sum " << sum
+//				<< " @ " << p2 << '\n';
 				p2++;
-				cout << "s " << sum << "\n";
-			}
-			if(sum==goal){
-				mins = goal;
-				break;
+				
 			}
 		
 			if(mins >= goal && sum >= goal)
 				mins = min(mins,sum);
 			else if(sum >= goal) mins = sum;
-//			mins = mins >= goal && sum >= goal ? min(mins,sum) : mins;
+			if(mins == goal) break;
 			sum -= rac[i];
-			cout << "mins " << mins << " -s " << sum << "\n";		
 		}
-		cout << "goal " << goal << "\n";
-//		cout << "ms " << mins << "\n";
-		if(mins>=goal){cout << mins << "\n";}
-//		else if(p2>c-1) cout << "IMPOSIBLE\n";
-//		else cout << "IMPOSIBLE\n";
-		if(mins >= goal) cout << "IMHERE";
-		cout << "m " << mins << " g " << goal << "\n";
+		if(mins < goal)
+			cout << "IMPOSIBLE\n";
+		else cout << mins << '\n';
+//		cout << "&&&&&&&&&&\n";
 
-//		cout << mins << "\n";
 		mins=0;sum=0;p2=0;
 		cin >> c >> goal; 
 	}
